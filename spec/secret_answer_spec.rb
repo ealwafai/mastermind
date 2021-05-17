75505e47f30ceb74aefba4be96f23c9f49edce10
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-05-17 17:24:00 -0600

    Build test for solution method

--- spec/secret_answer_spec.rb
+++ spec/secret_answer_spec.rb
@@ -8,5 +8,12 @@ describe SecretAnswer do
 
       expect(secret_answer).to be_an_instance_of(SecretAnswer)
     end
-  end 
+
+    it 'it creates array with 4 colors' do
+      secret_answer = SecretAnswer.new
+
+      expect(secret_answer.solution.length).to eq(4)
+      expect(secret_answer.solution).to include(:red).or include(:blue).or include(:yellow).or include(:green)
+    end
+  end
 end
