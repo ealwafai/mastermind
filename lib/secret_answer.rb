c4ff14c6f273e42ec68f488fb0cad642292a839b
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-05-17 17:23:19 -0600

    Create solution method

--- lib/secret_answer.rb
+++ lib/secret_answer.rb
@@ -1,5 +1,16 @@
 class SecretAnswer
 
+  attr_reader :answer
   def initialize
+  end
+
+  def solution
+    @answer = []
+    colors = [:red, :blue, :yellow, :green]
+    4.times do
+      index = rand(0..3)
+      @answer << colors[index]
+    end
+    @answer
   end
 end
