7acabaf4632e6ed396da78fd5409ee8c925815ec
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-05-18 19:43:40 -0600

    Build method to check guess

--- lib/guess_checker.rb
+++ lib/guess_checker.rb
@@ -1,8 +1,9 @@
 class GuessChecker
-attr_reader :guess
+attr_reader :guess, :answer
 
-  def initialize(guess)
+  def initialize(guess, answer)
     @guess = guess
+    @answer = answer
   end
 
   def length
@@ -23,5 +24,18 @@ attr_reader :guess
       end
     end
     result
+  end
+
+  def correct_colors
+    correct_colors = 0
+    index = 0
+    @guess.each do |color|
+      @answer.find do |color|
+        correct_colors += 1
+        @answer.shift[index]
+      end
+      index += 1
+    end
+    correct_colors
   end
 end
